2141160e769559ee6165b9e42d7549b2c08fa22f
Jordan Fisher <thementalgoose@gmail.com>
2025-11-26 20:27:32 +0000

    Re-adds files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Flashback for Pebble
+
+TODO
+
+#### Useful links
+
+- "Modular architecture" - https://github.com/pebble-examples/modular-app-example/blob/master/src/windows/main_window.h 
+- "Best Practices" - https://developer.rebble.io/guides/best-practices/modular-app-architecture/
---
+++ src/c/pebble-flashback.c
@@ -0,0 +1,27 @@
+#include <pebble.h>
+
+#include "modules/data.h"
+#include "modules/constants.h"
+#include "window/main-menu.h"
+#include "window/calendar.h"
+
+static void js_ready_callback() {
+  main_window_set_background_color(COLOR_FALLBACK(GColorGreen, GColorWhite));
+}
+
+static void init() {
+  const int array_size = 16;
+  data_init(array_size);
+
+  main_window_push();
+}
+
+static void deinit() {
+  data_deinit();
+}
+
+int main() {
+  init();
+  app_event_loop();
+  deinit();
+}
