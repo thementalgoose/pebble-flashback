c57d9bb4b2695b9a256b993e00fe3bda32867752
Jordan Fisher <thementalgoose@gmail.com>
2026-06-09 23:20:17 +0100

    Adds qualifying + race results for race screen

---
+++ src/c/windows/results_qualifying_window.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <pebble.h>
+
+// Create and show the qualifying results window
+void results_qualifying_window_push(int race_round);
+
+// Destroy the qualifying results window
+void results_qualifying_window_destroy(void);
